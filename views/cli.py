9cda25ad924ec40c46f480a3c26d3dd975b70972
Carloz1010 <carlosruizhernandez02@gmail.com>
2025-05-29 22:05:35 -0500

    Actualización del menú de opciones.

--- views/cli.py
+++ views/cli.py
@@ -10,10 +10,14 @@ def menu():
         print("\n--- Menú Principal ---")
         print("1. Importar datos desde API")
         print("2. Listar estaciones")
-        print("3. eliminar dato por id")
-        print("4. eliminar tabla")
-        print("5. listar por año")
-        print("6. Salir")
+        print("3. Eliminar dato por ID")
+        print("4. Eliminar tabla")
+        print("5. Listar por año")
+        print("6. Indices de fidelización por proveedores")
+        print("7. Resumen de cancelación y retención")
+        print("8. Grafica de retención por trimestre")
+        print("9. Grafica de cancelación por trimestre")
+        print("10. Salir")
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
@@ -29,13 +33,25 @@ def menu():
                   
         elif opcion == "4":
             controller.eliminar_tabla_estaciones()
+            
         
         elif opcion == "5":
-
             ano = int(input("Ingresa el año que desea listar: "))
             controller.listar_estaciones_por_ano(ano)
-            
+
         elif opcion == "6":
+            controller.indices_de_fidelizacion()
+
+        elif opcion == "7":
+            controller.mostrar_resumen_cancelacion_y_relacion()
+            
+        elif opcion == "8":
+            controller.graficar_retencion_por_trimestre()
+
+        elif opcion == "9":
+            controller.graficar_churnrate_por_trimestre()                        
+            
+        elif opcion == "10":
             print("Saliendo...")
             exit()
         else:
